56b76f577505c5be0228581357d8788dee3e76e7
carolross1 <carolrios347@gmail.com>
2024-08-24 17:27:40 -0600

    Ya se agrega la hora correcta

--- client/src/app/models/Entregas.ts
+++ client/src/app/models/Entregas.ts
@@ -1,9 +1,9 @@
 export interface Entrega {
-    id_Entrega: number;
-    id_Proveedor: number;
-    id_Factura: number;
-    fecha: Date;
-    hora: String;
-    total: number;
-    id_Usuario?: string; // Asegúrate de incluir la propiedad aquí
-  }
+  id_Entrega: number;
+  id_Proveedor: number;
+  id_Factura: number;
+  fecha: Date;
+  hora: String;
+  total: number;
+  id_Usuario?: string; // Asegúrate de incluir la propiedad aquí
+}
